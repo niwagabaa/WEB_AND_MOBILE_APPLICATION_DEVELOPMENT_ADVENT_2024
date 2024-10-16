e300ffee90fd819b13c3710a8556187cbd403d95
Roger <niwarogons@gmail.com>
2024-10-16 14:54:36 +0300

    I added some file that explain how the switch statements work in JS. That is

---
+++ Javascript/Activities/controlStructure.js
@@ -0,0 +1,22 @@
+//Switch statemen
+let score = 76;
+switch(score){
+    case 100:
+        console.log("A grade");
+        
+        break;
+    case 90:
+        console.log("B grade");
+        
+        break;
+    case 76:
+        console.log("C grade");
+        break;    
+        
+
+    default:
+
+}
+
+// If your working with ranges , If statements do the job.  
+//If your working with specific figures . switch statements do the ob
---
+++ Javascript/Activities/switch_statement.js
@@ -0,0 +1,21 @@
+//Question
+//What will be the result of the following code
+let score = 80;
+if (score > 90 && score <= 100) {
+    console.log("A grade");
+    
+}   else if (score > 75 && score <=90) {
+    console.log("B grade");
+    
+}   else if (score >=50 && score <- 75) {
+    console.log("C grade");
+    
+}   else {
+    console.log("Fail");
+    
+}
+
+//What will be the output of this code, and how can you simplify it using a swich statement
+
+// THe answer is in the controlStructure.js file
+
