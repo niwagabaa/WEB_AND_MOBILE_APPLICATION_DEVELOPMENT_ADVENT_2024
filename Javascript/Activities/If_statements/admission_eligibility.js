1e77c32949706266a4ad0b31c5b3d26e03120ae3
Roger <niwarogons@gmail.com>
2024-10-24 22:06:28 +0300

    I javascript program named admission eligibility which was a class exercise

---
+++ Javascript/Activities/If_statements/admission_eligibility.js
@@ -0,0 +1,14 @@
+//Exersice 2: Javascript program that checks if a person is eligible for admission in a school
+
+let score = 55;
+
+if (score >= 70) {
+    console.log("Eligible for Admission.");
+    
+} else if (score > 50 && score < 69) {
+    console.log("Admission on Probation.");
+    
+} else if (score < 50) {
+    console.log("Not Eligible for admission");
+
+} 
