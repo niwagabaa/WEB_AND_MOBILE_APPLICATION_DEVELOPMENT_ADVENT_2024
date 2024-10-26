9c98bd8590e89307f56e7e9f4dab0fc06aa06b6f
Roger <niwarogons@gmail.com>
2024-10-26 14:49:37 +0300

    Created a javascrript program that checks if the village has enough clean water for residents

---
+++ Javascript/Activities/If_statements/cleanWater.js
@@ -0,0 +1,14 @@
+//A js program that checks if the village has enough clean water for residents
+
+let waterAvailable = 700;
+
+if (waterAvailable > 500) {
+    console.log("Enough Water.");
+    
+} else if (waterAvailable > 200 && waterAvailable < 500) {
+    console.log("Water is limited");
+    
+} else if (waterAvailable < 200) {
+    console.log("Water shotage");
+    
+}
